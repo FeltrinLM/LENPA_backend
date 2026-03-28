1d2665738ae53d2293c4f6269e1614094ced3c37
FeltrinLM <lorenzofmendes49@gmail.com>
2026-03-28 20:45:08 -0300

    first commit

---
+++ src/main/java/com/example/lenpa_backend/LenpaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lenpa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenpaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LenpaBackendApplication.class, args);
+    }
+
+}
